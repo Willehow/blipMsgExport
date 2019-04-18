39a23d2c7a306189759e1b16da3d282b15f48345
BPVirtual <bpvirtualchat@gmail.com>
2019-04-18 17:14:24 -0300

    Separacao dos metodos

--- script/msg.js
+++ script/msg.js
@@ -12,58 +12,127 @@ exports.get = function(req, res) {
 
     var request = require('request');
 
-    var jsonBlip = {  
-        "id": "1",
-        "method": "get",
-        "uri": "/threads?$take=300"
+    var users = getThreads();
+
+    for(var i = 0; i < users["resource"]["items"].length;i++){
+
+        console.log(users["resource"]["items"][i].identity)
+
+        var user = users["resource"]["items"][i].identity
+
+        getMsg(user);
+        
     }
 
-    //Lets configure and request
-    request({
-        url: 'https://msging.net/commands', //URL to hit
-        method: 'POST', // specify the request type
-        headers: { // speciyfy the headers
-            'Content-Type': 'application/json',
-            'Authorization': 'Key cmgyOnJ4SmU1MktKbnRjWDhRRWRhQmdH'
-        },
-        body: JSON.stringify(jsonBlip) //Set the body as a string
-    }, function(error, response, body){
-        if(error) {
-            console.log(error);
-        } else {
-            console.log(response.statusCode, body);
+    buildCSV();
 
-            var users = JSON.parse(body)
 
-            console.log(users["from"])
+    function buildCSV(){
+        var json = [
 
-            /*
-            for(var i = 0; i<=users){
-
+            {
+            'login': 'f1648',
+            'nome': 'teste',
+            'email': 'teste@teste.com',
+            'msg': 'testando essa mensagem',
+            'data': '2019-04-12'
             }
-            */
+        ]
 
+        var opt = { fields, delimiter: ';', quote: ""}
+
+        var csv = json2csv(json, opt);
+
+        res.set("Content-Disposition", "attachment;filename=blipmsg.csv");
+        res.set("Content-Type", "application/octet-stream");
+
+        res.send(csv);
+    }
+
+    function getThreads(){
+
+        var jsonBlip = {  
+            "id": generateUUID(),
+            "method": "get",
+            "uri": "/threads?$take=50"
         }
-    });
 
-    var json = [
-        
-        {
-        'login': 'f1648',
-        'nome': 'teste',
-        'email': 'teste@teste.com',
-        'msg': 'testando essa mensagem',
-        'data': '2019-04-12'
+        //Lets configure and request
+        request({
+            url: 'https://msging.net/commands', //URL to hit
+            method: 'POST', // specify the request type
+            headers: { // speciyfy the headers
+                'Content-Type': 'application/json',
+                'Authorization': 'Key cmgyOnJ4SmU1MktKbnRjWDhRRWRhQmdH'
+            },
+            body: JSON.stringify(jsonBlip) //Set the body as a string
+        }, function(error, response, body){
+            if(error) {
+                console.log(error);
+            } else {
+                //console.log(response.statusCode, body);
+
+                var users = JSON.parse(body)
+
+                console.log(users["resource"]["items"].length)
+
+                return users;
+
+                /*
+                for(var i = 0; i < users["resource"]["items"].length;i++){
+
+                    console.log(users["resource"]["items"][i].identity)
+
+                    var user = users["resource"]["items"][i].identity
+
+                    getMsg(user);
+                    
+                }
+                */
+            }
+        });
+    }
+
+    function getMsg(user){
+
+        var jsonBlip2 = {  
+            "id": generateUUID(),
+            "method": "get",
+            "uri": "/threads/"+user
         }
-    ]
 
-    var opt = { fields, delimiter: ';', quote: ""}
- 
-    var csv = json2csv(json, opt);
- 
-    res.set("Content-Disposition", "attachment;filename=blipmsg.csv");
-    res.set("Content-Type", "application/octet-stream");
- 
-    res.send(csv);
- 
+        //Lets configure and request
+        request({
+            url: 'https://msging.net/commands', //URL to hit
+            method: 'POST', // specify the request type
+            headers: { // speciyfy the headers
+                'Content-Type': 'application/json',
+                'Authorization': 'Key cmgyOnJ4SmU1MktKbnRjWDhRRWRhQmdH'
+            },
+            body: JSON.stringify(jsonBlip2) //Set the body as a string
+        }, function(error, response, body){
+            if(error) {
+                console.log(error);
+            } else {
+
+                //console.log(response.statusCode, body);
+
+                var msgs = JSON.parse(body)
+
+                console.log(msgs)
+
+        }});
+    }
+
+    function generateUUID() { // Public Domain/MIT
+        var d = new Date().getTime();
+        if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+            d += performance.now(); //use high-precision timer if available
+        }
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+    }
 };
