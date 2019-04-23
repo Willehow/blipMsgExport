f9f64362ebbf6c2122f6f916b969fa66e82cf2ce
André Lima <37313063+Willehow@users.noreply.github.com>
2019-04-23 17:54:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var proxyUrl = "http://bprspws-wcg:8080";
-process.env.HTTP_PROXY = proxyUrl;
-process.env.HTTPS_PROXY = proxyUrl;
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+//var proxyUrl = "http://bprspws-wcg:8080";
+//process.env.HTTP_PROXY = proxyUrl;
+//process.env.HTTPS_PROXY = proxyUrl;
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 require('dotenv').config({silent: true});
 
