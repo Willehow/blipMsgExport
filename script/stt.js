e5f38ca72d42ea5f39aa57f441bfc83edb1e13c4
André Lima <37313063+Willehow@users.noreply.github.com>
2019-05-20 19:40:00 -0300

    Update stt.js

--- script/stt.js
+++ script/stt.js
@@ -6,38 +6,32 @@ const speechToText = new SpeechToTextV1({
   iam_apikey: 'xasydygb0RJ4rThfN_O4Tngfp9vbXUc1f_1zhEHUSt8e',
 });
 
+
 exports.post = function (req, res) {
 
-  var fileName = "audio.ogg";
-
-  options = {
-    url: req.body.url
-  };
-
-  var req = request(options)
-  req.on('data', function (res) {
-
-    fs.writeFileSync(fileName, res)
-
-  })
-
-  const recognizeParams = {
-    model: 'pt-BR_NarrowbandModel',
-
-    audio: fs.createReadStream(fileName),
-    content_type: 'audio/ogg'/*,
-        word_alternatives_threshold: 0.9,
-        keywords: ['colorado', 'tornado', 'tornadoes'],
-        keywords_threshold: 0.5,*/
-  };
-
-  speechToText.recognize(recognizeParams)
-    .then(speechRecognitionResults => {
-      //console.log(JSON.stringify(speechRecognitionResults, null, 2));
-      res.send(JSON.stringify(speechRecognitionResults, null, 2))
+  var audioFile = fs.createWriteStream('audio.ogg');
+  var url = req.body.url;
+  request(url)
+    .pipe(audioFile)
+    .on('finish', function(){
+      const recognizeParams = {
+        model: 'pt-BR_NarrowbandModel',
+        //audio: fs.createReadStream('C:\\Users\\f1648\\Downloads\\d1ba1905-bd12-4cc1-b262-d01c939bab38.oga'),
+        //audio: file,
+        audio: fs.createReadStream('audio.ogg'),
+        content_type: 'audio/ogg'/*,
+          word_alternatives_threshold: 0.9,
+          keywords: ['colorado', 'tornado', 'tornadoes'],
+          keywords_threshold: 0.5,*/
+      };
+  
+      speechToText.recognize(recognizeParams)
+        .then(speechRecognitionResults => {
+          console.log(JSON.stringify(speechRecognitionResults, null, 2));
+          res.send(JSON.stringify(speechRecognitionResults, null, 2))
+        })
+        .catch(err => {
+          console.log('error:', err);
+        });
     })
-    .catch(err => {
-      console.log('error:', err);
-    });
-
 };
